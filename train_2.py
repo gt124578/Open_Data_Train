fdc8342bccf4873a4ae53d434a29c937199e1d48
gt124578 <97751719+gt124578@users.noreply.github.com>
2025-09-09 11:51:20 +0200

    Update train_2.py

--- train_2.py
+++ train_2.py
@@ -7,7 +7,48 @@ from geopy.distance import geodesic
 conn = sqlite3.connect("tgvmax.db")
 cur = conn.cursor()
 
+"""with open("tgvmax.csv", newline='', encoding='utf-8') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    print("Colonnes détectées :", reader.fieldnames)"""
 
+
+# Suppression de l'ancienne table si elle existe 
+cur.execute("DROP TABLE IF EXISTS tgvmax_trajets")
+
+# Création de la nouvelle table avec la colonne Date
+cur.execute("""
+CREATE TABLE tgvmax_trajets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Date TEXT,
+    TRAIN_NO TEXT,
+    Origine TEXT,
+    Destination TEXT,
+    Heure_depart TEXT,
+    Heure_arrivee TEXT
+)
+""")
+conn.commit()
+
+def importer_csv(fichier):
+    with open(fichier, newline='', encoding='utf-8') as f:
+        reader = csv.DictReader(f, delimiter=';')
+        for row in reader:
+            cur.execute("""
+            INSERT INTO tgvmax_trajets (
+                "DATE", TRAIN_NO, Origine, Destination, Heure_depart, Heure_arrivee
+            ) VALUES (?, ?, ?, ?, ?, ?)
+            """, (
+                row['\ufeffDATE'],
+                row['TRAIN_NO'],
+                row['Origine'],
+                row['Destination'],
+                row['Heure_depart'],
+                row['Heure_arrivee']
+            ))
+    conn.commit()
+
+#Nom de la base de données : tgvmax.csv (doit être dans le même fichier que le code)
+importer_csv(r"tgvmax.csv")
 
 def SQL(a):
     cur.execute(a)
